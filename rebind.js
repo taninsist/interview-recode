731cea02bd460d8fbde8f8b409007c10c007b246
taning <taninsist@qq.com>
2024-10-09 18:38:18 +0800

    fix: 修改 myBind Api

--- rebind.js
+++ rebind.js
@@ -2,10 +2,15 @@
  * 手写 bind
  * Function.prototype.bind
  */
-Function.prototype.reBind = function (context, ...arg) {
+Function.prototype.myBind = function (context, ...args1) {
   const fn = this;
-  return () => {
-    fn.apply(context, [...arg]);
+  return function (...args2) {
+    // 判断是否是通过 new 关键字调用
+    if (this instanceof fn) {
+      return new fn(...args1, ...args2);
+    } else {
+      return fn.apply(context, [...args1, ...args2]);
+    }
   };
 };
 
@@ -13,9 +18,9 @@ const obj = {
   n: 40,
 };
 
-function test(b) {
-  console.log(this.n + b);
+function test(a, b, c, d) {
+  console.log(this.n, a, b, c, d);
 }
 
-const newTest = test.reBind(obj, 20);
-newTest();
+const newTest = test.myBind(obj, 20);
+newTest(1, 2, 3, 4, 6);
